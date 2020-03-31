a656b4152c2984757e87864676964007a2934987
Zeeshan-khan27 <62757911+Zeeshan-khan27@users.noreply.github.com>
2020-03-31 12:16:26 +0530

    Update sheduling.cpp

--- sheduling.cpp
+++ sheduling.cpp
@@ -1,3 +1,7 @@
+//If a teacher is being served at the food mess and during the period when he is being 
+//served, another teacher comes, then that teacher would get the service (food) next. This 
+//process might continue leading to increase in waiting time of students to get food. Ensure 
+//in your program that the waiting time of students is minimized.
 #include<stdio.h>
 #include<string.h>
 int main()
